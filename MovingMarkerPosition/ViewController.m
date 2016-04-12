bdb6d6d612af3822c1ee6a5581c810290256971a
Carlos Hwa <carloshwa@gmail.com>
2016-04-12 09:44:45 -0600

    Implement Place Autocomplete search

--- MovingMarkerPosition/ViewController.m
+++ MovingMarkerPosition/ViewController.m
@@ -2,55 +2,56 @@
 @import GoogleMaps;
 @import CoreLocation;
 
-@interface ViewController () <GMSIndoorDisplayDelegate, GMSMapViewDelegate>
+@interface ViewController () <GMSMapViewDelegate, GMSAutocompleteViewControllerDelegate>
 
 @property (strong, nonatomic) IBOutlet GMSMapView *mapView;
+- (IBAction)animateButton:(id)sender;
 @property (nonatomic, strong) GMSMarker *marker;
-@property (nonatomic, strong) NSTimer *timer;
-
--(void) updateMarkerDetails;
-
 @end
 
 @implementation ViewController
 
 -(void)viewDidAppear:(BOOL)animated {
   [super viewDidAppear:animated];
-  
-  CLLocationCoordinate2D location = CLLocationCoordinate2DMake(40.295210, -124.032841);
-  GMSCameraPosition *camera = [GMSCameraPosition cameraWithTarget:location zoom:10.5];
-  [self.mapView animateToCameraPosition:camera];
-  [self.mapView setMapType:kGMSTypeHybrid];
-  
-  self.marker = [GMSMarker markerWithPosition:location];
-  self.marker.map = self.mapView;
-  
-  // Move marker about
-  self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0
-                                                target:self
-                                              selector:@selector(updateMarkerDetails)
-                                              userInfo:nil
-                                               repeats:YES];
-  
-  // Seed random
-  srand48(arc4random());
 }
 
 #pragma mark - ViewController class extension methods
 
--(void) updateMarkerDetails {
-  
-  // Move the marker in a random direction.
-  double latDelta = (drand48() - 0.5) / 20;
-  double lngDelta = (drand48() - 0.5) / 20;
-  self.marker.position = CLLocationCoordinate2DMake(self.marker.position.latitude + latDelta,
-                                                    self.marker.position.longitude + lngDelta);
-  
-  // Make the map follow the marker.
-  CLLocationCoordinate2D newTarget = CLLocationCoordinate2DMake(self.mapView.camera.target.latitude + latDelta/3,
-                                                                self.mapView.camera.target.longitude + lngDelta/3);
-  GMSCameraPosition *position = [GMSCameraPosition cameraWithTarget:newTarget zoom:10.5];
-  [self.mapView animateToCameraPosition:position];
+- (IBAction)animateButton:(id)sender {
+    GMSAutocompleteViewController *acController = [[GMSAutocompleteViewController alloc] init];
+    acController.delegate = self;
+    [self presentViewController:acController animated:YES completion:nil];
 }
 
+// Handle the user's selection.
+- (void)viewController:(GMSAutocompleteViewController *)viewController
+didAutocompleteWithPlace:(GMSPlace *)place {
+    [self dismissViewControllerAnimated:YES completion:nil];
+
+    [self.mapView animateWithCameraUpdate:[GMSCameraUpdate setTarget:place.coordinate zoom:8.0f]];
+    
+    self.marker.map = nil;
+    self.marker = [GMSMarker markerWithPosition:place.coordinate];
+    self.marker.map = self.mapView;
+}
+
+- (void)viewController:(GMSAutocompleteViewController *)viewController
+didFailAutocompleteWithError:(NSError *)error {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    NSLog(@"Error: %@", [error description]);
+}
+
+// User canceled the operation.
+- (void)wasCancelled:(GMSAutocompleteViewController *)viewController {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+// Turn the network activity indicator on and off again.
+- (void)didRequestAutocompletePredictions:(GMSAutocompleteViewController *)viewController {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+- (void)didUpdateAutocompletePredictions:(GMSAutocompleteViewController *)viewController {
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
 @end
